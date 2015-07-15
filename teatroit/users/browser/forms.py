5b4eb2c81752acee7c9089342f6450e4f01bc68a
Davide Moro <davide.moro@gmail,.com>
2015-07-15 15:28:48 +0200

    add 3 different registration forms

--- teatroit/users/browser/forms.py
+++ teatroit/users/browser/forms.py
@@ -4,6 +4,9 @@ from plone.app.users.browser.register import RegistrationForm
 from quintagroup.formlib.captcha import CaptchaWidget
 from .interfaces import (
     IBasicRegistrationForm,
+    IUtenteRegistration,
+    ITeatroRegistration,
+    ICompagniaRegistration,
     ICaptchaSchema,
     )
 
@@ -14,16 +17,51 @@ class BasicRegistrationForm(RegistrationForm):
 
     implements(IBasicRegistrationForm)
 
+    def _addCaptcha(self, fields):
+        fields += form.Fields(ICaptchaSchema)
+        fields['captcha'].custom_widget = CaptchaWidget
+
+
+class UtenteRegistrationForm(BasicRegistrationForm):
     @property
     def form_fields(self):
         # Get the fields so we can fiddle with them
         myfields = super(BasicRegistrationForm, self).form_fields
 
         # Add a captcha field to the schema
-        myfields += form.Fields(ICaptchaSchema)
-        myfields['captcha'].custom_widget = CaptchaWidget
+        myfields += form.Fields(IUtenteRegistration)
 
-        # Perform any field shuffling here...
+        self._addCaptcha(myfields)
+
+        # Return the fiddled fields
+        return myfields
+
+
+class TeatroRegistrationForm(BasicRegistrationForm):
+    @property
+    def form_fields(self):
+        # Get the fields so we can fiddle with them
+        myfields = super(BasicRegistrationForm, self).form_fields
+
+        # Add a captcha field to the schema
+        myfields += form.Fields(ITeatroRegistration)
+
+        self._addCaptcha(myfields)
+
+        # Return the fiddled fields
+        return myfields
+
+
+class CompagniaRegistrationForm(BasicRegistrationForm):
+    @property
+    def form_fields(self):
+        # Get the fields so we can fiddle with them
+        myfields = super(BasicRegistrationForm, self).form_fields
+
+        # Add a captcha field to the schema
+        myfields += form.Fields(ICompagniaRegistration)
+
+        self._addCaptcha(myfields)
 
         # Return the fiddled fields
         return myfields
--- teatroit/users/browser/interfaces.py
+++ teatroit/users/browser/interfaces.py
@@ -1,4 +1,5 @@
 from zope.interface import Interface
+from zope import schema
 from quintagroup.formlib.captcha import Captcha
 from .. import teatroitUsersMessageFactory as _
 
@@ -15,3 +16,18 @@ class ICaptchaSchema(Interface):
             u'Type the code from the picture shown below.'
         ),
     )
+
+
+class IUtenteRegistration(Interface):
+    provincia = schema.TextLine(title=u"provincia", description=u"")
+
+
+class ITeatroRegistration(Interface):
+    location = schema.TextLine(title=u"location", description=u"")
+    comune = schema.TextLine(title=u"comune", description=u"")
+    provincia = schema.TextLine(title=u"provincia", description=u"")
+    cap = schema.TextLine(title=u"cap", description=u"")
+    telefono = schema.TextLine(title=u"telefono", description=u"")
+
+class ICompagniaRegistration(Interface):
+    tipo = schema.TextLine(title=u"tipo", description=u"")
