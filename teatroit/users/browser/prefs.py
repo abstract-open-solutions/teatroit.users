60d5e2f1927a1c6c0da20f60113c707cf92278a0
Simone Orsi <simahawk@gmail.com>
2015-08-04 12:53:41 +0200

    override user-information view to hide/show fields based on roles

--- teatroit/users/browser/prefs.py
+++ teatroit/users/browser/prefs.py
@@ -2,9 +2,49 @@ import copy
 
 from zope.component import getMultiAdapter
 from plone.app.users.browser.personalpreferences import UserDataPanel
+from plone.app.users.browser.personalpreferences import UserDataConfiglet
 from Products.Five.browser import BrowserView
 
 from .. import _
+
+
+class CustomizedUserDataConfiglet(UserDataConfiglet):
+
+    def __call__(self):
+        # we must do this on __call__ to get current member
+        # because on init security stuff is not available yet!
+        # XXX: we shoul make this machinery configurable
+        # per user type and roles
+        omitted_fields = [
+            'pdelete',
+            'compagnia_tipo',
+            'location',
+            'comune',
+            'cap',
+            'telefono',
+            'home_page',
+            'redazione_argomento'
+        ]
+        membership = getMultiAdapter((self.context, self.request),
+                                     name=u'plone_tools').membership()
+
+        if self.userid:
+            member = membership.getMemberById(self.userid)
+        else:
+            member = membership.getAuthenticatedMember()
+
+        if member and 'Redattore' in member.getRoles():
+            omitted_fields = [x for x in omitted_fields
+                              if not x.startswith('redazione_')]
+            for x in ('comune',
+                      'cap',
+                      'telefono',):
+                omitted_fields.remove(x)
+        if member and 'Compagnia' in member.getRoles():
+            omitted_fields = [x for x in omitted_fields
+                              if not x.startswith('compagnia_')]
+        self.form_fields = self.form_fields.omit(*omitted_fields)
+        return super(CustomizedUserDataConfiglet, self).__call__()
 
 
 class CustomizedUserDataPanel(BrowserView):
